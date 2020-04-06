909d2654ca494096a29d93678537ee49bc5c4c56
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-04-06 09:59:27 +0530

    Create 7th-Case.py

---
+++ 7th-Case.py
@@ -0,0 +1,23 @@
+'''
+
+Write a Python Program to print the given string in the format specified in the sample output.
+
+
+WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a
+SOVEREIGN, SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all
+its citizens
+
+'''
+#CODE
+
+print("WE, THE PEOPLE OF INDIA,\n\thaving solemnly resolved to constituteIndia into a SOVEREIGN,!")
+print("\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC \n\t\t and to secure to all its citizens")
+
+'''
+OUTPUT :
+
+WE, THE PEOPLE OF INDIA,
+      having solemnly resolved to constitute India into a SOVEREIGN, !
+            SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC
+             and to secure to all its citizens
+ '''
