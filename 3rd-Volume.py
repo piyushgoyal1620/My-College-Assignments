0e9660a8036a2cca7c74d2af92363c3c4e177569
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-04-06 09:45:05 +0530

    Create 3rd-Volume.py

---
+++ 3rd-Volume.py
@@ -0,0 +1,18 @@
+'''
+
+Write a Python program to find the volume of a sphere with diameter 12 cm.
+Formula: V=4/3 * π * r 3
+
+'''
+#CODE
+
+dia=int(input("please enter the length of diameter  "))
+print("volume of this sphere is ", ((4/3)*3.14*((dia/2)**3)))
+
+'''
+OUTPUT :
+
+please enter the length of diameter  12
+volume of this sphere is  904.3199999999999
+
+'''
