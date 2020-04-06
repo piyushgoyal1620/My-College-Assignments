76b4e2c628a06d2ec9b96164f5c2238d145b68c1
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-04-06 09:47:25 +0530

    Create 4th-list.py

---
+++ 4th-list.py
@@ -0,0 +1,15 @@
+'''
+
+Write a program which accepts a sequence of comma-separated numbers from console and
+generate a list.
+
+'''
+#CODE
+
+print(list(map(int,input().split(","))))
+
+'''
+INPUT : 1,2,3,5,7,8,9,65,102,547,854,12,54
+OUTPUT :  [1, 2, 3, 5, 7, 8, 9, 65, 102, 547, 854, 12, 54]
+
+'''
