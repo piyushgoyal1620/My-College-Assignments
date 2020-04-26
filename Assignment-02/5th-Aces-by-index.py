3485f76b24d8e7ba43ad3bb0902fc9cc50d9f62c
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-04-26 14:10:18 +0530

    Create 5th-Aces-by-index.py

---
+++ Assignment-02/5th-Aces-by-index.py
@@ -0,0 +1,13 @@
+#Write a Python program to access dictionary key’s element by index.
+
+# code
+
+Piyush = {'stats': 80, 'math': 90, 'algorithm': 86}
+print(list(Piyush)[0])
+
+'''
+output
+
+stats
+
+'''
