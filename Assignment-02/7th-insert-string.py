91e5f47efcaac160841aefc271fdc8813fd67723
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-04-26 14:15:22 +0530

    Create 7th-insert-string.py

---
+++ Assignment-02/7th-insert-string.py
@@ -0,0 +1,15 @@
+#write a program to insert a string at the begining of every elements in a list.
+
+#CODE 
+
+PIYUSH = [100,123,345,567,789,890,98,876,543,678]
+#enter customer before each element
+print(['customer{0}'.format(i) for i in PIYUSH])
+
+'''
+OUTPUT
+
+['customer100', 'customer123', 'customer345', 'customer567', 'customer789', 'customer890', 'customer98', 
+'customer876', 'customer543', 'customer678']
+
+'''
