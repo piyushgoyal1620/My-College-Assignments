22feeddcaa022bda85c00ed4b1bd4397169c59ee
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-04-26 14:07:59 +0530

    Create 4th-find-highest-list.py

---
+++ Assignment-02/4th-find-highest-list.py
@@ -0,0 +1,13 @@
+#Write a Python program to find the list in a list of lists whose sum of elements is the highest
+
+# code
+
+Piyush = [[1,2,3], [4,5,6], [10,11,12], [7,8,9]]
+print(max(Piyush, key=sum))
+
+'''
+output
+
+[10, 11, 12]
+
+'''
